fb8d93d3131cbfa66540fe7fd13a9be3b3210c3c
SarahTCC <sarah@thecommerce.co>
2023-11-03 15:33:58 +1100

    Remove styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  resources :widgets
   resources :sarahs
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
   root 'sarahs#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
